ed0f1877325a4ec9f06eb5a86ec7011054c3d4b2
Joseph Curley <hey.josephcurley@gmail.com>
2020-02-07 10:27:36 -0500

    Card Components first pass

---
+++ src/components/card/card-editorial/card-editorial.stories.js
@@ -0,0 +1,53 @@
+import "../card.scss";
+
+export default { title: 'Cards' };
+
+const data = {
+	header: "Large Editorial Card",
+	description: "Have you ever heard a museum love story? Did you know that art has the power to heal? Met visitors share their personal stories in this new series.",
+	images: "https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&hash=9CDD1BCFB213A815CCF4B476CDA5B35F 2x, https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&w=1920&hash=342B752D9534482E6C5C988C117585A4 1x",
+	link: {
+		url: "http://metmuseum.org",
+		text: "Watch Episode 1"
+	}
+}
+
+const cardMarkup = (model) => {
+	return `<section class="editorial-card--large">
+		<div class="card">
+			<a href="${model.link.url}" class="card-image__link" tabindex="-1">
+				<div class="card-image__wrapper">
+					<img class="card-image" srcset="${model.images}">
+				</div>
+			</a>
+			<div class="card-subject">
+				<div class="card-subject__body">
+					<div class="card-header">
+						<div class="card-header__body">
+							<h2 class="card-header__title">
+								<a href="${model.link.url}">
+									${model.header}
+								</a>
+							</h2>
+						</div>
+					</div>
+					<div class="card-meta">
+						<div class="card-meta__body">
+							<div class="card-meta__description">
+								${model.description}
+							</div>
+							<div class="card-meta__link">
+								<a href="${model.link.url}" class="button--tertiary">${model.link.text}</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>`
+}
+
+
+export const LargeEdiotrialCard = () => {
+  return cardMarkup(data);
+}
---
+++ src/components/card/card-multiple/card-multiple.stories.js
@@ -0,0 +1,83 @@
+import "../card.scss";
+
+export default { title: 'Cards' };
+
+const cardTemplate = (
+	cardHeader = "Large Editorial Card",
+	descriptor = "descriptor",
+	description = "description",
+	images = "https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&hash=9CDD1BCFB213A815CCF4B476CDA5B35F 2x, https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&w=1920&hash=342B752D9534482E6C5C988C117585A4 1x",
+	link = {
+		url: "http://metmuseum.org",
+		text: "Watch Episode 1"
+	}
+) => {
+	return {
+		header: cardHeader,
+		descriptor: descriptor,
+		description: description,
+		images: images,
+		link: {
+			url: link.url,
+			text: link.text
+		}
+	}
+};
+
+const data = {
+	header: "MultiCards",
+	cards: [
+		cardTemplate(),
+		cardTemplate("Another Card", undefined, "This is a second Card"),
+		cardTemplate()
+	]
+};
+
+const cardMarkup = (model) => {
+	return `<section class="${model.cards.length} multicard">
+  <h3>${model.header}</h3>
+	<div class="multicard-wrapper">
+	  ${ model.cards.reduce((total, card) => {
+			return total + `<div class="card card--active">
+	      <div class="card-image__wrapper">
+	        <a href="${card.link.url}" class="card-image__link" tabindex="-1">
+	          <img class="card-image" srcset="${card.images}">
+	        </a>
+	      </div>
+	      <div class="card-subject">
+	        <div class="card-subject__body card-subject__body--multicard">
+	          <div class="card-header">
+	            <div class="card-header__body">
+	              <div class="card-header__descriptor">
+	                <p>
+	                  ${card.descriptor}
+	                </p>
+	              </div>
+	              <h3 class="card-header__heading">
+	                <a href="${card.link.url}">
+	                  ${card.header}
+	                </a>
+	              </h3>
+	            </div>
+	          </div>
+	          ${ card.description &&
+		          `<div class="card-meta">
+		            <div class="card-meta__body">
+		              <div class="card-meta__description">
+		                ${card.description}
+		              </div>
+		            </div>
+		          </div>`
+	          }
+	        </div>
+	      </div>
+	    </div>`
+		}, "")}
+	</div>
+</section>`
+}
+
+
+export const MultipleCards = () => {
+  return cardMarkup(data);
+}
