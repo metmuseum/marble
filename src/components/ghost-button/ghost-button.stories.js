97cf652a7e18710411b3db9e58ff844355209e93
Stephen Marsh <stephenforpresident@gmail.com>
2020-03-30 19:26:11 -0400

    comment out stories til v1.0, so it won't blow up for now

--- src/components/ghost-button/ghost-button.stories.js
+++ src/components/ghost-button/ghost-button.stories.js
@@ -1,12 +1,12 @@
-import { withKnobs, text } from "@storybook/addon-knobs";
-import "./ghost-button.scss"
+// import { withKnobs, text } from "@storybook/addon-knobs";
+// import "./ghost-button.scss"
 
-export default {
-  title: 'Ghost Button',
-  decorators: [withKnobs]
-};
+// export default {
+//   title: 'Ghost Button',
+//   decorators: [withKnobs]
+// };
 
-export const small = () => `<button class="ghost-button ghost-button--small">${text("Label", "Primary")}</button>`;
-export const smallDisabled = () => `<button class="ghost-button ghost-button--small" disabled>${text("Label", "Disabled")}</button>`;
-export const large = () => `<button class="ghost-button ghost-button--large">${text("Label", "Primary")}</button>`;
-export const largeDisabled = () => `<button class="ghost-button ghost-button--large" disabled>${text("Label", "Disabled")}</button>`;
+// export const small = () => `<button class="ghost-button ghost-button--small">${text("Label", "Primary")}</button>`;
+// export const smallDisabled = () => `<button class="ghost-button ghost-button--small" disabled>${text("Label", "Disabled")}</button>`;
+// export const large = () => `<button class="ghost-button ghost-button--large">${text("Label", "Primary")}</button>`;
+// export const largeDisabled = () => `<button class="ghost-button ghost-button--large" disabled>${text("Label", "Disabled")}</button>`;
