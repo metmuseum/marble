2d50bec35070a6eab3697a4424e9d09af3b80f9c
Joseph Curley <hey.josephcurley@gmail.com>
2020-04-03 13:31:37 -0400

    Revert "Commenting out Story until v1"
    This reverts commit 9acbbdfb63e4f42c80a7969923c57db4a886874e.

--- src/components/notification-banner/notification-banner.stories.js
+++ src/components/notification-banner/notification-banner.stories.js
@@ -1,36 +1,35 @@
-// import "./notification-banner.scss";
-//
-// export default { title: 'Notification Banner' };
-//
-// const data = {
-// 	header: "The Museum Has Temporarily Closed",
-// 	description: "The Museum has temporarily closed all three locations effective March 13, to support New York City’s effort to contain the spread of COVID-19.",
-// 	link: {
-// 		url: "http://metmuseum.org",
-// 		text: "Learn More"
-// 	}
-// }
-//
-// const notificationMarkup = (model) => {
-// 	return `<section>
-// 		<div class="notification-banner ${model.className}">
-// 			<h2 class="notification-banner__header">${model.header}</h2>
-// 			<div class="notification-banner__body">
-// 				<p class="notification-banner__subtext">${model.description}</p>
-// 				<a href="${model.link.url}" class="notification-banner__link">${model.link.text}</a>
-// 			</div>
-//
-// 		</div>
-// 	</section>`
-// }
-//
-//
-// export const expressive = () => {
-// 	let model = Object.assign(data, {className: "notification-banner--expressive"});
-//   return notificationMarkup(model);
-// }
-//
-// export const productive = () => {
-// 	let model = Object.assign(data, {className: "notification-banner--productive"});
-//   return notificationMarkup(model);
-// }
+import "./notification-banner.scss";
+
+export default { title: 'Notification Banner' };
+
+const data = {
+	header: "The Museum Has Temporarily Closed",
+	description: "The Museum has temporarily closed all three locations effective March 13, to support New York City’s effort to contain the spread of COVID-19.",
+	link: {
+		url: "http://metmuseum.org",
+		text: "Learn More"
+	}
+}
+
+const notificationMarkup = (model) => {
+	return `<section>
+		<div class="notification-banner ${model.className}">
+			<h2 class="notification-banner__header">${model.header}</h2>
+			<div class="notification-banner__body">
+				<p class="notification-banner__subtext">${model.description}</p>
+				<a href="${model.link.url}" class="notification-banner__link">${model.link.text}</a>
+			</div>
+		</div>
+	</section>`
+}
+
+
+export const expressive = () => {
+	let model = Object.assign(data, {className: "notification-banner--expressive"});
+  return notificationMarkup(model);
+}
+
+export const productive = () => {
+	let model = Object.assign(data, {className: "notification-banner--productive"});
+  return notificationMarkup(model);
+}
