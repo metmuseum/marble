af0248ebbc841411bd30cb32691f5d9533106a53
David Seguin <david.seguin@gmail.com>
2020-04-16 15:38:57 -0400

    Flesh out headings in storybook (#16)
    * a code review

--- src/global/global.stories.js
+++ src/global/global.stories.js
@@ -2,16 +2,24 @@ import "./global.scss";
 
 export default { title: 'Typography' };
 
-const h1Expressive = (text) => `<h1 class="expressive">${text}</h1>`;
 const h1 = (text) => `<h1>${text}</h1>`;
+const h1Fixed = (text) => `<div class="fixed-width"><h1>${text}</h1></div>`;
+const h1Expressive = (text) => `<h1 class="expressive">${text}</h1>`;
 const h2 = (text) => `<h2>${text}</h2>`;
+const h2Fixed = (text) => `<div class="fixed-width"><h2>${text}</h2></div>`;
+const h2Subtext = (text) => `<h2 class="notification-banner__subtext">${text}</h2>`;
+const h2SubtextFixed = (text) => `<div class="fixed-width"><h2 class="notification-banner__subtext">${text}</h2></div>`;
 const h3 = (text) => `<h3>${text}</h3>`;
 const h4 = (text) => `<h4>${text}</h4>`;
 
 export const headings = () => {
-  return h1Expressive("An Expressive h1 - the biggest heading we offer in Marble") +
-  h1("A normal H1. Wow!") +
-  h2("A humble H2") +
-  h3("H3 - The heading of the people") +
-  h4("That's right folks. We even go down to h4s")
+  return h1("By Jove, my quick study of lexicography won a prize!") +
+  h1Fixed("By Jove, my quick study of lexicography won a prize!") +
+  h1Expressive("By Jove, my quick study of lexicography won a prize!") +
+  h2("By Jove, my quick study of lexicography won a prize!") +
+  h2Fixed("By Jove, my quick study of lexicography won a prize!") +
+  h2Subtext("By Jove, my quick study of lexicography won a prize!") +
+  h2SubtextFixed("By Jove, my quick study of lexicography won a prize!") +
+  h3("By Jove, my quick study of lexicography won a prize!") +
+  h4("By Jove, my quick study of lexicography won a prize!") +
 }
