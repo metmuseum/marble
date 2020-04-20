735aa97cd5e7920e472d8ad8b48569a94b2e2ea5
Stephen Marsh <stephenforpresident@gmail.com>
2020-04-20 16:42:48 -0400

    update buttons raw helper to html helper [DIGAUD-3229]

--- src/global/buttons.stories.js
+++ src/global/buttons.stories.js
@@ -1,4 +1,4 @@
-import raw from "../../.storybook/helpers/html";
+import html from "../../.storybook/helpers/html";
 import { withKnobs, text } from "@storybook/addon-knobs";
 import "./buttons.scss";
 
@@ -8,31 +8,31 @@ export default {
 };
 
 export const tertiary = () =>
-	raw`<button class="button--tertiary">
-    ${text("Label", "Tertiary")}
-  </button>
-  <br />`;
+	html`<button class="button--tertiary">
+			${text("Label", "Tertiary")}
+		</button>
+		<br />`;
 
 export const tertiaryDisabled = () =>
-	raw`
-  <button class="button--tertiary" disabled>
-    ${text("Label", "Tertiary Disabled")}
-  </button>
-  <br />
-  `;
+	html`
+		<button class="button--tertiary" disabled>
+			${text("Label", "Tertiary Disabled")}
+		</button>
+		<br />
+	`;
 
 // TODO: automated tests to ensure <button> and <a> styles are always identical.
 export const anchorTagTertiaryButton = () =>
-	raw`
-  <a role="button" tabindex="0" class="button--tertiary">
-    ${text("Label", "Anchor Tag Styled As Tertiary Button")}
-  </a>
-  <br />
-  `;
+	html`
+		<a role="button" tabindex="0" class="button--tertiary">
+			${text("Label", "Anchor Tag Styled As Tertiary Button")}
+		</a>
+		<br />
+	`;
 
 export const anchorTagTertiaryButtonDisabled = () =>
-	raw`
-  <a role="button" tabindex="0" class="button--tertiary" disabled>
-    ${text("Label", "Anchor Tag Styled As Tertiary Button Disabled")}
-  </a>
-  `;
+	html`
+		<a role="button" tabindex="0" class="button--tertiary" disabled>
+			${text("Label", "Anchor Tag Styled As Tertiary Button Disabled")}
+		</a>
+	`;
