e336e0f0ada1ff8d41e41630db3de20c775137d7
Joseph Curley <hey.josephcurley@gmail.com>
2020-05-01 11:17:09 -0400

    Fleshing out HTML template

--- src/components/featured-card/featured-card.stories.js
+++ src/components/featured-card/featured-card.stories.js
@@ -1,10 +1,12 @@
+import html from "../../../.storybook/helpers/html";
 import "./featured-card.scss";
-
-export default { title: 'Featured Card' };
+// import { withA11y } from "@storybook/addon-a11y";
+import { fullWidth } from "../image-container/image-container.stories.js";
+export default { title: 'Featured' };
 
 const data = {
-	header: "Featured Card",
-	description: "This Represents a Marble Card without any tweaking. Currently Not really a component, just the basis of a bunch of different ones.",
+	header: "Visit Us from Home",
+	description: "Immerse yourself in 360-degree video of iconic spaces in the Museum. Visit the Great Hall as a ball,  and Greek as a treat!",
 	images: "https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&hash=9CDD1BCFB213A815CCF4B476CDA5B35F 2x, https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&w=1920&hash=342B752D9534482E6C5C988C117585A4 1x",
 	link: {
 		url: "http://metmuseum.org",
@@ -13,34 +15,11 @@ const data = {
 }
 
 const featuredCardMarkup = (model) => {
-	return `<section>
-		<div class="marble-card">
-			<a href="${model.link.url}" class="marble-card__image-link" tabindex="-1">
-				<div class="marble-card__image-wrapper">
-					<img class="marble-card__image" srcset="${model.images}">
-				</div>
-			</a>
-			<div class="marble-card__subject">
-				<div class="marble-card__subject-body">
-					<h2 class="marble-card__header">
-						<a href="${model.link.url}">
-							${model.header}
-						</a>
-					</h2>
-					<div class="marble-card__meta">
-						<div class="marble-card__meta-body">
-							<div class="marble-card__meta-description">
-								${model.description}
-							</div>
-							<div class="marble-card__meta-link">
-								<a href="${model.link.url}" class="button--tertiary">${model.link.text}</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>`
+	return html`
+	<div class="featured-card">
+		<h1>${model.header}</h1>
+		<div class="featured-card__description">${model.description}</div>
+	</div>`
 }
 
 
