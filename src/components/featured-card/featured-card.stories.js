a70286eaebf8fcfd84c3f7fb53ee4d78d1d3d77a
Joseph Curley <hey.josephcurley@gmail.com>
2020-05-01 13:16:02 -0400

    Featured-card.... created.

--- src/components/featured-card/featured-card.stories.js
+++ src/components/featured-card/featured-card.stories.js
@@ -1,24 +1,38 @@
 import html from "../../../.storybook/helpers/html";
 import "./featured-card.scss";
-// import { withA11y } from "@storybook/addon-a11y";
+
 import { fullWidth } from "../image-container/image-container.stories.js";
+
 export default { title: 'Featured' };
 
 const data = {
 	header: "Visit Us from Home",
-	description: "Immerse yourself in 360-degree video of iconic spaces in the Museum. Visit the Great Hall as a ball,  and Greek as a treat!",
-	images: "https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&hash=9CDD1BCFB213A815CCF4B476CDA5B35F 2x, https://www.metmuseum.org/-/media/images/150-anniversary/met-stories/2020_met_stories_ep_01_4k_new.jpg?la=en&w=1920&hash=342B752D9534482E6C5C988C117585A4 1x",
-	link: {
+	description: "<p>Immerse yourself in 360-degree video of iconic spaces in the Museum. Visit the Great Hall as a ball,  and Greek as a treat!</p>",
+	images: "",
+	primaryLink: {
 		url: "http://metmuseum.org",
-		text: "Watch Episode 1"
+		text: "Primary"
+	},
+	secondaryLink: {
+		url: "http://metmuseum.org",
+		text: "Browse Artwork"
 	}
 }
 
 const featuredCardMarkup = (model) => {
 	return html`
 	<div class="featured-card">
-		<h1>${model.header}</h1>
-		<div class="featured-card__description">${model.description}</div>
+		<div class="featured-card__content">
+			<h1 class="expressive">${model.header}</h1>
+			<div class="featured-card__description">${model.description}</div>
+			<div class="featured-card__links">
+				<a href="#" class="button button--ghosted inverse">${model.primaryLink.text}</a>
+				<a href="#" class="button--tertiary inverse featured-card__secondary-button">${model.secondaryLink.text}</a>
+			</div>
+		</div>
+		<div class="featured-card__image-wrapper">
+			${fullWidth()}
+		</div>
 	</div>`
 }
 
