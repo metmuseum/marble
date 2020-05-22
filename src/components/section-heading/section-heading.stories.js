6e4516ba095eaf5b739fd3776e01d7c280680d6c
Stephen Marsh <stephenforpresident@gmail.com>
2020-05-22 11:35:44 -0400

    fix heading style of section heading component

--- src/components/section-heading/section-heading.stories.js
+++ src/components/section-heading/section-heading.stories.js
@@ -37,10 +37,10 @@ export const sectionHeading = () => {
 	return html`
 		<div
 			class="section-heading section-heading--text-${textAlignment} ${inSitu
-				? "productive-component"
-				: ""}"
+			? "productive-component"
+			: ""}"
 		>
-			<h1 class="section-heading__heading ${context}">${header}</h1>
+			<h2 class="section-heading__heading ${context}">${header}</h2>
 			<div>${he.decode(bodyCopy)}</div>
 			<a
 				class="button--tertiary section-heading__text-link"
