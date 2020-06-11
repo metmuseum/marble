96f5e1f72c94d59b82e7792b9d9fb9a95d3c3bcf
David Seguin <david.seguin@gmail.com>
2020-06-11 16:49:55 -0400

    toggler

--- src/components/toggler/toggler.js
+++ src/components/toggler/toggler.js
@@ -5,7 +5,15 @@ export default function theToggler() {
     togglers.forEach((toggler) => {
 
 		toggler.onclick = function(){
-			toggler.parentNode.querySelector('.js-toggled-closed').classList.add('js-toggled-open');
+            //toggler.parentNode.querySelector('.js-toggled-closed').classList.add('js-toggled-open');
+            var toBeToggled = toggler.parentNode.querySelector('.js-togglee');
+            if (toBeToggled.classList.contains('js-toggled-closed')) {
+                toBeToggled.classList.remove('js-toggled-closed');
+                toBeToggled.classList.add('js-toggled-open');
+            } else {
+                toBeToggled.classList.add('js-toggled-closed');
+                toBeToggled.classList.remove('js-toggled-open');
+            }
 		};
 	});
 
