c386a0c9abdddd71b0c8cf756f8a1eaa786f3460
David Seguin <david.seguin@gmail.com>
2020-06-12 09:56:26 -0400

    sidebar typography

--- src/components/toggler/toggler.js
+++ src/components/toggler/toggler.js
@@ -5,7 +5,6 @@ export default function theToggler() {
     togglers.forEach((toggler) => {
 
 		toggler.onclick = function(){
-            //toggler.parentNode.querySelector('.js-toggled-closed').classList.add('js-toggled-open');
             var toBeToggled = toggler.parentNode.querySelector('.js-togglee');
             if (toBeToggled.classList.contains('js-toggled-closed')) {
                 toBeToggled.classList.remove('js-toggled-closed');
@@ -16,5 +15,4 @@ export default function theToggler() {
             }
 		};
 	});
-
 }
