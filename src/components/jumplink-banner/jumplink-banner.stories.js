a393004f8dbe9e2066a3cd3630ab9c007503a40a
Joseph Curley <hey.josephcurley@gmail.com>
2020-06-15 12:21:12 -0400

    Created Jump Link Banner

---
+++ src/components/jumplink-banner/jumplink-banner.stories.js
@@ -0,0 +1,62 @@
+import html from "../../../.storybook/helpers/html";
+import he from "he";
+import { withA11y } from "@storybook/addon-a11y";
+import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import { fullWidth } from "../image-container/image-container.stories.js";
+import "./jumplink-banner.scss";
+
+export default { title: "Banner", decorators: [withA11y, withKnobs] };
+
+const data = () => {
+	return {
+		inSitu: boolean("In Situ", false),
+		leftAlign: boolean("Left Align", false),
+		bottomAlign: boolean("Bottom Align", false),
+		header: text("Header", "Visit Us from Home"),
+		description: text(
+			"Body Copy",
+			"<p>Immerse yourself in 360-degree video of iconic spaces in the Museum. Visit the Great Hall as a ball,  and Greek as a treat!</p>"
+		),
+		images: "",
+		links: [
+			{
+				url: "http://metmuseum.org",
+				text: text("Button One Text", "Primary Link"),
+			},
+			{
+				url: "http://metmuseum.org",
+				text: text("Button Two Text", "Component two"),
+			},
+			{
+				url: "http://metmuseum.org",
+				text: text("Button Three Text", "Footer"),
+			}
+		]
+	};
+};
+
+const jumplinkBannerMarkup = (model) => {
+	return html` <div
+		class="jumplink-banner
+			${model.inSitu ? "productive-component" : ""}
+			${model.leftAlign ? "align--left" : ""}
+			${model.bottomAlign ? "align--bottom" : ""}
+	">
+		<div class="jumplink-banner__content">
+			<h1 class="expressive">${model.header}</h1>
+			<h3 class="jumplink-banner__description">${he.decode(model.description)}</h3>
+			<div class="jumplink-banner__links">
+			${model.links.map((link) => `
+				 <a href="${link.url}" class="button--wide jumplink-banner__link button button--pill inverse">${link.text}</a>
+			 `).join('')}
+			</div>
+		</div>
+		<div class="jumplink-banner__image-wrapper">
+			${fullWidth()}
+		</div>
+	</div>`;
+};
+
+export const JumpLinkBanner = () => {
+	return jumplinkBannerMarkup(data());
+};
