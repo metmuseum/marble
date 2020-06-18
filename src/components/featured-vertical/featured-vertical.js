ab898a3e1342af9ae6c163644ed7e0e94e24bb4e
David Seguin <david.seguin@gmail.com>
2020-06-18 12:13:02 -0400

    update poster changer

--- src/components/featured-vertical/featured-vertical.js
+++ src/components/featured-vertical/featured-vertical.js
@@ -20,11 +20,16 @@ export default function featuredVerticalFlickity() {
 	const posterChangers = document.querySelectorAll('.js-poster-changer');
 	const initialPosterChanger = document.querySelector('.js-poster-changer');
 	const initialPoster = document.querySelector('.js-poster');
+	const initialPosterLink = initialPosterChanger.querySelector('.article-card__header-heading-link').href;
+	const initialTitle = initialPosterChanger.querySelector('.article-card__header-heading-link').text;
 	const posterFrame = document.querySelector('#poster-frame');
 	const posters = posterFrame.querySelectorAll('.js-poster');
 
+	//set the initial state
 	initialPosterChanger.classList.add('poster-changer-active');
 	initialPoster.classList.add('image-opaque');
+	posterFrame.href = initialPosterLink;
+	posterFrame.title = initialTitle;
 
 	const deactivateAllPosterChangers = (e) => {
 		posterChangers.forEach(posterChanger => {
@@ -38,18 +43,21 @@ export default function featuredVerticalFlickity() {
 		});
 	}
 
-	const changePoster = (theIndex) => {
+	const changePoster = (theIndex, theLink, theTitle) => {
 		deactivateAllPosters();
 		posters[theIndex].classList.add('image-opaque');
+		posterFrame.href = theLink;
+		posterFrame.title = theTitle;
 	}
 
 	posterChangers.forEach((posterChanger) => {
-
 		posterChanger.onmouseover = function(theIndex){
 			var thisPosterChangerIndex = [...this.closest('.featured-vertical-items__wrapper').querySelectorAll('.js-poster-changer')].indexOf(this);
+			var thisPosterChangerLink = this.querySelector('.article-card__header-heading-link').href;
+			var thisPosterChangerTitle = this.querySelector('.article-card__header-heading-link').text;
 			deactivateAllPosterChangers();
 			this.classList.add('poster-changer-active');
-			changePoster(thisPosterChangerIndex);
+			changePoster(thisPosterChangerIndex, thisPosterChangerLink, thisPosterChangerTitle);
 		};
 	});
 }
