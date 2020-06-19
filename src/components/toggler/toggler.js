3456453851ee13ffdfb2dbec33787b95fb9c38e6
David Seguin <david.seguin@gmail.com>
2020-06-19 16:41:48 -0400

    article sidebar

--- src/components/toggler/toggler.js
+++ src/components/toggler/toggler.js
@@ -5,7 +5,7 @@ export default function theToggler() {
     togglers.forEach((toggler) => {
 
 		toggler.onclick = function(){
-            var toBeToggled = toggler.parentNode.querySelector('.js-togglee');
+            var toBeToggled = toggler.parentNode;
             if (toBeToggled.classList.contains('js-toggled-closed')) {
                 toBeToggled.classList.remove('js-toggled-closed');
                 toBeToggled.classList.add('js-toggled-open');
