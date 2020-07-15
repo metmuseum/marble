576d6d54dc04fac3add6dfafa6369df690ad51a6
Stephen Marsh <stephenforpresident@gmail.com>
2020-07-15 16:13:05 -0400

    wip adding knobs for button modes [skip-ci]

--- src/global/buttons/primary/filled/button.stories.js
+++ src/global/buttons/primary/filled/button.stories.js
@@ -1,18 +1,73 @@
 import html from "../../../../../.storybook/helpers/html";
-import { withKnobs, text } from "@storybook/addon-knobs";
+import { withKnobs, text, radios, boolean } from "@storybook/addon-knobs";
 import "../../../../marble.scss";
+
+const sizeSelector = (defaultValue = "large") => {
+	const label = "Size Modes";
+	const options = {
+		Small: "small",
+		Large: "large",
+	};
+
+	return radios(label, options, defaultValue);
+};
+
+const stateSelector = (defaultValue = "active") => {
+	const label = "State";
+	const options = {
+		Active: "active",
+		Inactive: "inactive",
+	};
+	return radios(label, options, defaultValue);
+};
 
 export default {
 	title: "Buttons/Primary/Filled/Button Tag",
 	decorators: [withKnobs],
 };
 
-export const LargeActive = () =>
-	html`<button class="button primary-button primary-button--large">
-		${text("Label", "Primary Button")}
+export const LargeActive = () => {
+	const sizeClass = `primary-button--${sizeSelector()}`;
+	const disabledOption = stateSelector() === "inactive" ? "disabled" : "";
+	return html`<button
+		class="button primary-button ${sizeClass}"
+		${disabledOption}
+	>
+		${text("Label", "Primary")}
 	</button>`;
+};
 
-export const LargeInactive = () =>
-	html`<button class="button primary-button primary-button--large" disabled>
-		${text("Label", "Primary Button")}
+export const LargeInactive = () => {
+	const sizeClass = `primary-button--${sizeSelector()}`;
+	const disabledOption =
+		stateSelector("inactive") === "inactive" ? "disabled" : "";
+	return html`<button
+		class="button primary-button ${sizeClass}"
+		${disabledOption}
+	>
+		${text("Label", "Primary")}
 	</button>`;
+};
+
+export const SmallActive = () => {
+	const sizeClass = `primary-button--${sizeSelector("small")}`;
+	const disabledOption = stateSelector() === "inactive" ? "disabled" : "";
+	return html`<button
+		class="button primary-button ${sizeClass}"
+		${disabledOption}
+	>
+		${text("Label", "Primary")}
+	</button>`;
+};
+
+export const SmallInactive = () => {
+	const sizeClass = `primary-button--${sizeSelector("small")}`;
+	const disabledOption =
+		stateSelector("inactive") === "inactive" ? "disabled" : "";
+	return html`<button
+		class="button primary-button ${sizeClass}"
+		${disabledOption}
+	>
+		${text("Label", "Primary")}
+	</button>`;
+};
