674a7097e9f77807673e8b0cdcd93e61be6991bc
David Seguin <david.seguin@gmail.com>
2020-08-04 14:14:48 -0400

    remove window on load

--- src/components/deep-look/deep-look.js
+++ src/components/deep-look/deep-look.js
@@ -1,61 +1,62 @@
 import OpenSeadragon from "OpenSeadragon";
 
 export default function deepLookViewer() {
-    window.onload = function () {
-        const containerElement = document.querySelector(".js-deep-look__image");
-        const imageURL = containerElement.dataset.image;
-        const viewer = OpenSeadragon({
-            id: "deep-look__image",
-            prefixUrl: "openseadragon/images/",
-            tileSources: {
-                type: "image",
-                url: imageURL,
-            },
-            gestureSettingsMouse: {
-                clickToZoom: false,
-                scrolltoZoom: false,
-                pinchtoZoom: false,
-            },
-            visibilityRatio: 1,
-            mouseNavEnabled: false,
-            showNavigationControl: false,
-            homeFillsViewer: true,
-            minZoomLevel: 1,
-            animationTime: 3,
-        });
+    console.log('GOGOGOGOG');
 
-        let homeZoom = viewer.viewport.getHomeZoom();
-        document.querySelector('.js-deep-look-home').setAttribute('data-z', homeZoom);
-        document.querySelector('.js-deep-look-home').setAttribute('data-x', '0.5');
-        document.querySelector('.js-deep-look-home').setAttribute('data-y', '0.7');
+    const containerElement = document.querySelector(".js-deep-look__image");
+    const imageURL = containerElement.dataset.image;
+    const viewer = OpenSeadragon({
+        id: "deep-look__image",
+        prefixUrl: "openseadragon/images/",
+        tileSources: {
+            type: "image",
+            url: imageURL,
+        },
+        gestureSettingsMouse: {
+            clickToZoom: false,
+            scrolltoZoom: false,
+            pinchtoZoom: false,
+        },
+        visibilityRatio: 1,
+        mouseNavEnabled: false,
+        showNavigationControl: false,
+        homeFillsViewer: true,
+        minZoomLevel: 1,
+        animationTime: 3,
+    });
 
-        let essayEntries = document.querySelectorAll('.js-deep-look-zoom');
+    let homeZoom = viewer.viewport.getHomeZoom();
+    document.querySelector('.js-deep-look-home').setAttribute('data-z', homeZoom);
+    document.querySelector('.js-deep-look-home').setAttribute('data-x', '0.5');
+    document.querySelector('.js-deep-look-home').setAttribute('data-y', '0.7');
 
-        let config = {
-            root: null,
-            rootMargin: '0px',
-            threshold: 0.2
-        };
+    let essayEntries = document.querySelectorAll('.js-deep-look-zoom');
 
-        let observer = new IntersectionObserver(onChange, config);
-        essayEntries.forEach(entry => observer.observe(entry));
-
-        function onChange(chunks, observer) {
-            chunks.forEach(chunk => {
-                if (chunk.intersectionRatio > 0) {
-                    goToPoint(chunk.target);
-                }
-            });
-        }
-
-        function goToPoint(entry) {
-            var coordinates = {};
-            coordinates.x = parseFloat(entry.dataset.x);
-            coordinates.y = parseFloat(entry.dataset.y);
-            var zoomlevel = parseFloat(entry.dataset.z);
-
-            viewer.viewport.panTo(coordinates);
-            viewer.viewport.zoomTo(zoomlevel);
-        }
+    let config = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.2
     };
+
+    let observer = new IntersectionObserver(onChange, config);
+    essayEntries.forEach(entry => observer.observe(entry));
+
+    function onChange(chunks, observer) {
+        chunks.forEach(chunk => {
+            if (chunk.intersectionRatio > 0) {
+                goToPoint(chunk.target);
+            }
+        });
+    }
+
+    function goToPoint(entry) {
+        var coordinates = {};
+        coordinates.x = parseFloat(entry.dataset.x);
+        coordinates.y = parseFloat(entry.dataset.y);
+        var zoomlevel = parseFloat(entry.dataset.z);
+
+        viewer.viewport.panTo(coordinates);
+        viewer.viewport.zoomTo(zoomlevel);
+    }
+
 }
