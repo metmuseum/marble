fe2b7c16c5547395610493960eb033ffb55a1510
Stephen Marsh <stephenforpresident@gmail.com>
2020-08-18 14:52:25 -0400

    restyle topics card to h4

--- src/components/topics-slider/topics-card.stories.js
+++ src/components/topics-slider/topics-card.stories.js
@@ -10,29 +10,30 @@ export default { title: "Components/Topics Card", decorators: [withKnobs] };
 
 const TopicsCard = () => {
 	const linkTitle = text("Link Title", "This is a title");
-	return html`
-		<div class="topic-card topic-card--active">
-			<div class="topic-card__image-wrapper topic-card__image-wrapper--fixed-ratio topic-card__image-wrapper--66">
-				<a href="link" class="topic-card__image-link" tabindex="-1">
-					<img
-						class="topic-card__image"
-						alt="ALT"
-						src="${image768}"
-						srcset="${image768} 768w, ${image960} 960w, ${image1440} 1440w"
-						sizes="425px"
-					/>
-				</a>
+	return html` <div class="topic-card topic-card--active">
+		<div
+			class="topic-card__image-wrapper topic-card__image-wrapper--fixed-ratio topic-card__image-wrapper--66"
+		>
+			<a href="link" class="topic-card__image-link" tabindex="-1">
+				<img
+					class="topic-card__image"
+					alt="ALT"
+					src="${image768}"
+					srcset="${image768} 768w, ${image960} 960w, ${image1440} 1440w"
+					sizes="425px"
+				/>
+			</a>
+		</div>
+		<div class="topic-card__subject">
+			<div class="topic-card__header">
+				<h4 class="topic-card__header-heading">
+					<a href="link">
+						${linkTitle}
+					</a>
+				</h4>
 			</div>
-			<div class="topic-card__subject">
-				<div class="topic-card__header">
-					<h3 class="topic-card__header-heading">
-						<a href="link">
-							${linkTitle}
-						</a>
-					</h3>
-				</div>
-			</div>
-		</div>`;
+		</div>
+	</div>`;
 };
 
 export { TopicsCard };
