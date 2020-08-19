97a44a468572b68233d831907bc61a5c256061ca
Stephen Marsh <stephenforpresident@gmail.com>
2020-08-19 14:17:09 -0400

    add flickity css to topics slider

--- src/components/topics-slider/topics-slider.stories.js
+++ src/components/topics-slider/topics-slider.stories.js
@@ -1,26 +1,23 @@
 import html from "../../../.storybook/helpers/html";
+import { TopicsCard } from "./topics-card.stories.js";
+import topicsSlider from "./topics-slider.js";
+import { useEffect } from "@storybook/client-api";
 
-import {TopicsCard} from "./topics-card.stories.js"
-
-import { text, withKnobs } from "@storybook/addon-knobs";
+import { withKnobs } from "@storybook/addon-knobs";
 
 export default { title: "Components/Topics Slider", decorators: [withKnobs] };
 
 const TopicsSlider = () => {
-
-    return html`<section class="topics-slider">
-    <div class="stream__header">
-        <h2 class="featured-vertical__heading">Heading</h2>
-    </div>
-    <div class="topics-slider-list js-topics-slider-list">
-        ${TopicsCard()}
-        ${TopicsCard()}
-        ${TopicsCard()}
-        ${TopicsCard()}
-        ${TopicsCard()}
-        ${TopicsCard()}
-    </div>
-</section>`;
+	useEffect(topicsSlider);
+	return html`<section class="topics-slider">
+		<div class="stream__header">
+			<h2 class="featured-vertical__heading">Heading</h2>
+		</div>
+		<div class="topics-slider-list js-topics-slider-list">
+			${TopicsCard()} ${TopicsCard()} ${TopicsCard()} ${TopicsCard()}
+			${TopicsCard()} ${TopicsCard()}
+		</div>
+	</section>`;
 };
 
 export { TopicsSlider };
