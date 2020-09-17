43c91f99a26fc8f20c7058e3bdc06201526d901b
Stephen Marsh <stephenforpresident@gmail.com>
2020-09-17 15:07:14 -0400

    wire up byline styles

--- src/components/byline/byline.stories.js
+++ src/components/byline/byline.stories.js
@@ -1,27 +1,31 @@
 import html from "../../../.storybook/helpers/html";
+import withKnobs from "@storybook/addon-knobs";
 export default { title: "Components/Byline" };
+
+// export default {
+// 	title: "Byline",
+// 	addons: [withKnobs],
+// };
 
 const data = {
 	name: "Jeanie Choi",
 	date: "January 1, 1957",
 	link: {
 		url: "http://metmuseum.org",
-	}
-}
+	},
+};
 
 const cardMarkup = (model) => {
 	return html`<div class="byline">
-        <span class="byline__author">
-            <a href="${model.link.url}">${model.name}</a>
-        </span>
-        <span class="byline__date">
-            ${model.date}
-        </span>
-    </div>`;
+		<span class="byline__author">
+			<a href="${model.link.url}">${model.name}</a>
+		</span>
+		<span class="byline__date"> ${model.date} </span>
+	</div>`;
 };
 
-export { cardMarkup };
-
-export const Byline = () => {
-	return cardMarkup(data());
+const Byline = () => {
+	return cardMarkup(data);
 };
+
+export { Byline };
