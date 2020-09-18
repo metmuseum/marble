c99be72a076f436ade4ef3090a2dcdfef5749c04
Stephen Marsh <stephenforpresident@gmail.com>
2020-09-18 15:32:00 -0400

    add knob for multiple byline authors

--- src/components/byline/byline.stories.js
+++ src/components/byline/byline.stories.js
@@ -1,30 +1,39 @@
 import html from "../../../.storybook/helpers/html";
-//import withKnobs from "@storybook/addon-knobs";
-//export default { title: "Components/Byline" };
-
 import { number, withKnobs } from "@storybook/addon-knobs";
 
-export default { title: "Components/Byline", decorators: [withKnobs],};
+export default { title: "Components/Byline", decorators: [withKnobs] };
 
-const data = {
-	name: "Jeanie Choi",
-	date: "January 1, 1957",
-	link: {
-		url: "http://metmuseum.org",
-	},
+const authors = () => {
+	const numberOfAuthors = number("Number of Authors", 2); // get 2 authors from our numbers knob
+	let authors = [...Array(numberOfAuthors).keys()].map(() => {
+		return "Jeanie Choi";
+	}); // just make an array that just says "Jeanie Choi" that many times
+
+	return authors
+		.map((author) => {
+			return authorMarkup(author);
+		})
+		.join(", "); // return all the tags as 1 html string
 };
 
-const cardMarkup = (model) => {
+const authorMarkup = (author) => {
+	return html`<a class="byline__author-link" href="/">${author}</a>`;
+};
+
+const data = {
+	authors,
+	date: "January 1, 1957",
+};
+
+const bylineMarkup = (model) => {
 	return html`<div class="byline">
-		<span class="byline__author">
-			<a class="byline__author-link" href="${model.link.url}">${model.name}</a>
-		</span>
-		<span class="byline__date"> ${model.date} </span>
+		<span class="byline__authors">${model.authors()}</span>
+		<span class="byline__date">${model.date}</span>
 	</div>`;
 };
 
 const Byline = () => {
-	return cardMarkup(data);
+	return bylineMarkup(data);
 };
 
 export { Byline };
