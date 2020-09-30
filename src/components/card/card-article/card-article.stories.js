a2bdde730426ea96d1d347296e7e4886226587bd
Stephen Marsh <stephenforpresident@gmail.com>
2020-09-30 14:50:54 -0400

    wip pushing up some placeholders for our variables, attempting a knob

--- src/components/card/card-article/card-article.stories.js
+++ src/components/card/card-article/card-article.stories.js
@@ -1,15 +1,41 @@
 import html from "../../../../.storybook/helpers/html";
+import { Byline } from "../../byline/byline.stories";
+import { text, withKnobs } from "@storybook/addon-knobs";
+
+import image768 from "../../../../.storybook/assets/images/full-width-image/seurat_circus_sideshow.jpg";
+import image960 from "../../../../.storybook/assets/images/full-width-image/seurat_circus_sideshow-960.jpg";
+import image1440 from "../../../../.storybook/assets/images/full-width-image/seurat_circus_sideshow-1440.jpg";
+
+export default {
+	title: "Cards/Article Card",
+	addons: [withKnobs],
+};
 
 const ArticleCard = () => {
-	return html`
-	<div class="article-card article-card--active">
-		<div class="article-card__image-wrapper article-card__image-wrapper--fixed-ratio article-card__image-wrapper--66">
+	let link, alt, srcset, sizes, topic, title, description, byline;
+
+	link = "/";
+	srcset = html`${image768} 768w, ${image960} 960w, ${image1440} 1440w`;
+	sizes = "33vw";
+	// title = ;
+
+	return html`<div class="article-card article-card--active">
+		<div
+			class="article-card__image-wrapper article-card__image-wrapper--fixed-ratio article-card__image-wrapper--66"
+		>
 			<a href="${link}" class="article-card__image-link" tabindex="-1">
-				<img class="article-card__image" alt="${alt}" srcset="${scrset}">
+				<img
+					class="article-card__image"
+					alt="${alt}"
+					srcset="${srcset}"
+					sizes="${sizes}"
+				/>
 			</a>
 		</div>
 		<div class="article-card__subject">
-			<div class="article-card__subject-body article-card__subject-body--multicard">
+			<div
+				class="article-card__subject-body article-card__subject-body--multicard"
+			>
 				<div class="article-card__header">
 					<h4 class="article-card__header-descriptor">
 						<a class="article-card__header-descriptor-link" href="${link}">
@@ -17,9 +43,7 @@ const ArticleCard = () => {
 						</a>
 					</h4>
 					<h3 class="article-card__header-heading">
-						<a href="${link}">
-							${title}
-						</a>
+						<a href="${link}"> ${text("Title", "Boop")} </a>
 					</h3>
 				</div>
 				<div class="article-card__meta">
@@ -27,7 +51,7 @@ const ArticleCard = () => {
 						<div class="article-card__meta-description">
 							<p>${description}</p>
 						</div>
-						${byline}
+						${Byline()}
 					</div>
 				</div>
 			</div>
@@ -35,8 +59,4 @@ const ArticleCard = () => {
 	</div>`;
 };
 
-export default {
-	title: "Article Card"
-};
-
-export {ArticleCard};
+export { ArticleCard };
