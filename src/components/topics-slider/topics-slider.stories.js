98ba9105c7bd993117f090f7d0c7d31502d0dc61
David Seguin <david.seguin@gmail.com>
2020-10-27 12:07:43 -0400

    attempt a Carousel in a Slider

--- src/components/topics-slider/topics-slider.stories.js
+++ src/components/topics-slider/topics-slider.stories.js
@@ -1,18 +1,21 @@
 import html from "../../../.storybook/helpers/html";
 import { TopicsCard } from "./topics-card.stories.js";
-import topicsSlider from "./topics-slider.js";
+import carousel, { flickityDefaults } from "../carousel/carousel.js";
 import { useEffect } from "@storybook/client-api";
 import { withKnobs } from "@storybook/addon-knobs";
 
 export default { title: "Components/Topics Slider", decorators: [withKnobs] };
 
 const TopicsSlider = () => {
-	useEffect(topicsSlider);
-	return html`<section class="topics-slider">
+	// useEffect(()=> {
+	// 	carousel(object("Flickity Settings", flickityDefaults))
+	// });
+	return html`
+	<section class="topics-slider">
 		<div class="stream__header">
 			<h2 class="featured-vertical__heading">Heading</h2>
 		</div>
-		<div class="topics-slider-list js-topics-slider-list">
+		<div class="topics-slider-list js-carousel carousel">
 			${TopicsCard()} ${TopicsCard()} ${TopicsCard()} ${TopicsCard()}
 			${TopicsCard()} ${TopicsCard()}
 		</div>
