3701460246b1edf3113bf8b7d45295dbe15bba65
Stephen Marsh <stephenforpresident@gmail.com>
2020-10-28 16:16:34 -0400

    update variables in tab controls

--- src/components/tab-controls/tab-controls.stories.js
+++ src/components/tab-controls/tab-controls.stories.js
@@ -5,22 +5,25 @@ export default {
 };
 
 const TabControls = () => {
+	const screenreaderLegendText =
+		"Select between Past, Current, or Upcoming Exhibitions";
+	const tabNames = ["Past", "Current", "Upcoming"];
+	const inputGroupName = "my-cool-tabs"
+
 	return html`<fieldset class="tabs-control-container">
-		${["Past", "Current", "Upcoming"]
+		<legend class="screen-reader-only">${screenreaderLegendText}</legend>
+		${tabNames
 			.map((tabName, index) => {
 				return html` <div class="tab-controls">
 					<input
-						id="${tabName}-tab-select-${index}"
+						id="${tabName}-tab-id"
 						type="radio"
-						name="storybook-demo-tab-select"
+						name=${inputGroupName}
 						class="tab-controls__input"
 						value="${tabName}"
 						${index === 1 && "checked"}
 					/>
-					<label
-						for="${tabName}-tab-select-${index}"
-						class="tab-controls__label"
-					>
+					<label for="${tabName}-tab-id" class="tab-controls__label">
 						<h3>${tabName}</h3>
 					</label>
 				</div>`;
