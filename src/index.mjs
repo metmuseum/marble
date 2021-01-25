9f689a8a2d880c73fa994c7cd7a67a859a270dc1
Stephen Marsh <stephenforpresident@gmail.com>
2021-01-25 14:16:54 -0500

    remove css-in-js import of marble in the js entrypoint, update docs

--- src/index.mjs
+++ src/index.mjs
@@ -4,9 +4,7 @@
 //       examples:
 //         a) import jumpLinkBanner from "marble/jump-link-banner"
 //         b) import { jumpLinkBanner } from "marble"
-// TODO: consider bundling at all? we'd get better tree-shaking if we used  "module: './src/index.mjs'" but all clients then need webpack.
 
-import "./marble.scss";
 import global from "./global/global.mjs";
 import jumpLinkBanner from "./components/jumplink-banner/jumplink-banner.js";
 import vimeoPlayToggle from "./components/vimeo-player/vimeo-controls.js";
