62668a2ef709070634e0404ee1f16eca5b2ad2e3
Stephen Marsh <stephenforpresident@gmail.com>
2021-02-22 15:00:08 -0500

    fix a few spots

--- .storybook/helpers/backgroundOverride.js
+++ .storybook/helpers/backgroundOverride.js
@@ -1,9 +1,9 @@
 import html from "./html";
-import colorVariables from "../../src/base/_colors.scss";
+import scssExports from "../../src/global/exports.scss";
 
 // This is only needed because Chromatic does not (yet) support snapshot tests that respect the backgrounds add-on
 
-const backgroundOverride = (color = colorVariables.colorGrey500) => html`<style>
+const backgroundOverride = (color = scssExports.colorGrey500) => html`<style>
 	.sb-show-main {
 		background: ${color};
 	}
