0becadcc41ceeab09270b3d218fed5ff5c5d32a0
Joseph Curley <hey.josephcurley@gmail.com>
2021-03-03 11:18:41 -0500

    Robust handling of tabs

--- src/components/exhibition-detail-page/exhibition-tabs/exhibition-tabs.js
+++ src/components/exhibition-detail-page/exhibition-tabs/exhibition-tabs.js
@@ -1,31 +1,58 @@
 function exhibitionTabs() {
+
+	//Only one tab control should exist on a page.
+	const exhibitionTabComponent = document.querySelector(".js-edp-tabs");
+	const exhibitionTabs = Array.from(exhibitionTabComponent.querySelectorAll(".js-edp-tabs__tab"));
 	const exhibitionSections = document.querySelectorAll(".js-exhibition-section");
 
-	document.querySelectorAll(".js-edp-tabs").forEach((exhibitionTabComponent) => {
-		const showActiveTab = (activeTab) => {
-			exhibitionSections.forEach((section) => {
-				section.dataset.name === activeTab ? section.classList.add("is-active") : section.classList.remove("is-active");
-			});
+	//Get Initial URL params
+	let urlParams = new URLSearchParams(window.location.search);
 
-			exhibitionTabComponent.scrollIntoView({behavior: "smooth", inline: "nearest"});
-		};
-		const exhibitionTabs = exhibitionTabComponent.querySelectorAll(".js-edp-tabs__tab");
+	//Add a ?section=activeSection to the URL.
+	const handleURLupdate = (sectionName) => {
+		urlParams.set('section', sectionName);
+		const stringifiedUrlParams = urlParams.toString();
+		window.history.replaceState(null, null, `?${stringifiedUrlParams}`);
+	};
 
-		exhibitionTabs.forEach((tab)=> {
-			tab.addEventListener("click", (e) => {
-				e.preventDefault();
-				const sectionToOpen = tab.getAttribute("href");
+	//Hide deselected sections and show the selected one.
+	const handleSectionChange = (activeTab) => {
+		exhibitionSections.forEach((section) => {
+			section.dataset.name === activeTab ? section.classList.add("is-active") : section.classList.remove("is-active");
+		});
 
-				history.pushState({}, '', `?section=${sectionToOpen}`);
+		exhibitionTabComponent.scrollIntoView({behavior: "smooth", inline: "nearest"});
+		handleURLupdate(activeTab);
+	};
 
-				exhibitionTabs.forEach((tab) => tab.classList.remove("is-active"));
+	//Show active state on tab. Call section handler.
+	const handleTabChange = (tab) => {
+		const sectionToOpen = tab.getAttribute("href");
+		exhibitionTabs.forEach((tab) => tab.classList.remove("is-active"));
+		tab.classList.add("is-active");
+		handleSectionChange(sectionToOpen);
+	};
 
-				tab.classList.add("is-active");
+	//Check URL for an active section. If none exist select the first section.
+	const handleInitialState = () => {
 
-				showActiveTab(sectionToOpen);
-			});
+		const sectionName = urlParams.has("section") ?
+			urlParams.get("section") :
+			exhibitionTabs[0].getAttribute("href");
+
+		handleSectionChange(sectionName);
+		const activeTab = exhibitionTabs.find((tab) => tab.getAttribute("href") === sectionName);
+		activeTab.classList.add("is-active");
+	};
+
+	exhibitionTabs.forEach((tab)=> {
+		tab.addEventListener("click", (e) => {
+			e.preventDefault();
+			handleTabChange(tab);
 		});
 	});
+
+	handleInitialState();
 }
 
 export default exhibitionTabs;
