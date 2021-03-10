2c54b3a15945ffc064eeb13831b0458559d241be
Stephen Marsh <stephenforpresident@gmail.com>
2021-03-09 21:23:53 -0500

    make sure the scroll event only fires once before it is removed

--- src/global/lazyload/lazyload.js
+++ src/global/lazyload/lazyload.js
@@ -2,19 +2,25 @@ require(`intersection-observer`);
 import LazyLoad from "vanilla-lazyload";
 
 export default function lazyload() {
+	const imageLoadedEvent = new CustomEvent("image-loaded", { bubbles: true });
+
 	const lazyLoadObj = new LazyLoad({
 		elements_selector: ".lazy",
 		callback_loaded: (el) => {
-			el.dispatchEvent(new CustomEvent("image-loaded", { bubbles: true }));
+			el.dispatchEvent(imageLoadedEvent);
 		},
 	});
 
-	//on scroll load all below the fold Images
-	window.addEventListener("scroll", loadImages, true);
-	//After they load unbind the scroll listener.
 	function loadImages() {
-		console.log("this ran");
 		lazyLoadObj.loadAll();
+		// After they load unbind the scroll listener.
 		window.removeEventListener("scroll", loadImages, true);
 	}
+
+	//on scroll load all below the fold Images
+	window.addEventListener("scroll", loadImages, {
+		once: true,
+		passive: true,
+		capture: true,
+	});
 }
