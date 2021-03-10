f0ead0ebdf487fc079d677ae49a69c03a9b9270a
Stephen Marsh <stephenforpresident@gmail.com>
2021-03-09 21:10:52 -0500

    remove cruft

--- src/global/lazyload/lazyload.js
+++ src/global/lazyload/lazyload.js
@@ -2,18 +2,18 @@ require(`intersection-observer`);
 import LazyLoad from "vanilla-lazyload";
 
 export default function lazyload() {
-	// console.log("global lazy load temporarily disabled");
 	const lazyLoadObj = new LazyLoad({
 		elements_selector: ".lazy",
-		callback_loaded: el => {
+		callback_loaded: (el) => {
 			el.dispatchEvent(new CustomEvent("image-loaded", { bubbles: true }));
-		}
+		},
 	});
 
 	//on scroll load all below the fold Images
 	window.addEventListener("scroll", loadImages, true);
 	//After they load unbind the scroll listener.
 	function loadImages() {
+		console.log("this ran");
 		lazyLoadObj.loadAll();
 		window.removeEventListener("scroll", loadImages, true);
 	}
