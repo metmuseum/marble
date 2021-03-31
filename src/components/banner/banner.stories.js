f1e555f7d7adfb427dd876cd019cd0c9cec3d0d4
Stephen Marsh <stephenforpresident@gmail.com>
2021-03-31 11:49:10 -0400

    check more images into codebase to fix banner tests

--- src/components/banner/banner.stories.js
+++ src/components/banner/banner.stories.js
@@ -3,47 +3,52 @@ import vimeoTemplate from "../vimeo-player/vimeo-player";
 import vimeoControls from "../vimeo-player/vimeo-controls.js";
 import { useEffect } from "@storybook/client-api";
 
-export default {
-	title: "Banner",
-	parameters: {
-		chromatic: { delay: 1500 },
-	},
-};
+import image712 from "../../../.storybook/assets/images/misc/150HubBannerMobile_712w.jpg";
+import image1231 from "../../../.storybook/assets/images/misc/150HubBannerMobile_1231w.jpg";
+
+export default { title: "Banner" };
 
 const data = {
 	backgroundColor: "#ECDFD7",
 	color: "#333333",
 	header: "Celebrating 150 Years",
-	description: "<p>A storied past. A dynamic present. An exciting future. It's just the beginning!</p><p> Celebrate The Met's anniversary throughout 2020 with exhibitions, events, and new ways to connect with art. Highlights include Making The Met, 1870–2020, the reimagined British Galleries, and a three-day celebration in June.</p>",
-	backgroundImages: "https://www.metmuseum.org/-/media/images/150-anniversary/150hubbannermobile.jpg?la=en&hash=507F0AA67DD7FDBB3AFF148741E8A2A0 2x, https://www.metmuseum.org/-/media/images/150-anniversary/150hubbannermobile.jpg?la=en&w=712&hash=3A9DFAFD734E0FAEC4EF53205B45476D 1x",
+	description:
+		"<p>A storied past. A dynamic present. An exciting future. It's just the beginning!</p><p> Celebrate The Met's anniversary throughout 2020 with exhibitions, events, and new ways to connect with art. Highlights include Making The Met, 1870–2020, the reimagined British Galleries, and a three-day celebration in June.</p>",
+	backgroundImages: `${image1231} 2x, ${image712} 1x`,
 	imageAlt: "Celebrating 150 Years",
 	link: {
 		url: "http://metmuseum.org",
-	}
-}
-
+	},
+};
 
 const bannerMarkup = (model) => {
 	return html`
 		<section class="marble-banner marble-banner-@Model.Name">
 			<div class="marble-banner__image-wrapper">
-				<a class="marble-banner__image-link"
+				<a
+					class="marble-banner__image-link"
 					tabindex="-1"
 					href="${model.link.url}"
-					title="${model.imageAlt}">
-
-					<img class="marble-banner__image"
+					title="${model.imageAlt}"
+				>
+					<img
+						class="marble-banner__image"
 						srcset="${model.backgroundImages}"
-						alt="${model.imageAlt}">
+						alt="${model.imageAlt}"
+					/>
 
 					${model.video === true ? vimeoTemplate : ``}
 				</a>
-
 			</div>
-			<div class="marble-banner__subject" style="background-color: ${model.backgroundColor}; color: ${model.color}">
+			<div
+				class="marble-banner__subject"
+				style="background-color: ${model.backgroundColor}; color: ${model.color}"
+			>
 				<div class="marble-banner__subject-body">
 					<h1 class="expressive">
-						<a href="${model.link.url}" class="marble-banner__header-link">${model.header}</a>
+						<a href="${model.link.url}" class="marble-banner__header-link"
+							>${model.header}</a
+						>
 					</h1>
 					<div class="marble-banner__meta-description expressive-body">
 						<a href="${model.link.url}" tabindex="-1">${model.description}</a>
@@ -51,17 +56,16 @@ const bannerMarkup = (model) => {
 				</div>
 			</div>
 		</section>
-`
-}
-
+	`;
+};
 
 export const Banner = () => {
-	let model = Object.assign(data, {video: false});
-  return bannerMarkup(model);
-}
+	let model = Object.assign(data, { video: false });
+	return bannerMarkup(model);
+};
 
 export const BannerWithVideo = () => {
 	useEffect(vimeoControls);
-	const model = Object.assign(data, {video: true});
-  return bannerMarkup(model);
-}
+	const model = Object.assign(data, { video: true });
+	return bannerMarkup(model);
+};
