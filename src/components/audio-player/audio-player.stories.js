d1f3dfaa7a76d2260d0c92177d9fc955c7c6138b
Stephen Marsh <stephen.marsh@metmuseum.org>
2021-06-07 12:51:53 -0400

    add cover image to audio player

--- src/components/audio-player/audio-player.stories.js
+++ src/components/audio-player/audio-player.stories.js
@@ -2,6 +2,9 @@ import html from "../../../.storybook/helpers/html";
 import { useEffect } from "@storybook/client-api";
 import audioPlayer from "./audio-player.js";
 import { withKnobs, text, boolean } from "@storybook/addon-knobs";
+import greekHall1x1 from "../../../.storybook/assets/images/greek-hall/1x1";
+
+
 export default { title: "Media/Audio Player", decorators: [withKnobs] };
 
 const audioPlayerMarkUp = (model) => {
@@ -11,12 +14,17 @@ const audioPlayerMarkUp = (model) => {
 
 	return html`
 		<div style="padding: 40px; background-color: #eee">
-
 			<section class="audio-player js-marble-audio-player ${playerModes}">
-
 				<div class="audio-player__media-section">
 					<div class="audio-player__image-section">
 						<div class="audio-player__image-wrapper">
+							<img
+								class="audio-player__cover-image"
+								alt="${model.track.coverImage.alt}"
+								width="${model.track.coverImage.width}"
+								height="${model.track.coverImage.height}"
+								src="${model.track.coverImage.srcSet.fallback}"
+							/>						
 						</div>
 					</div>
 
@@ -30,27 +38,32 @@ const audioPlayerMarkUp = (model) => {
 								<audio
 									style="width: 100%; height: 36px;"
 									controls
-									src="${model.track.audioFile}">
-								</audio>
+									src="${model.track.audioFile}"
+								></audio>
 							</div>
 						</div>
 					</div>
-
 				</div>
 
-				<div class="audio-player__transcript-section js-audio-player__transcript-section">
-					<div class="audio-player__transcript-wrapper js-audio-player__transcript-wrapper">
+				<div
+					class="audio-player__transcript-section js-audio-player__transcript-section"
+				>
+					<div
+						class="audio-player__transcript-wrapper js-audio-player__transcript-wrapper"
+					>
 						<div class="audio-player__transcript">
 							<p>${model.track.transcript}</p>
 						</div>
 					</div>
-					<a href="#" class="audio-player__transcript-toggle js-audio-player__transcript-toggle">
+					<a
+						href="#"
+						class="audio-player__transcript-toggle js-audio-player__transcript-toggle"
+					>
 						<span class="transcript__toggle-icon"> &#9660;</span>
 						View Transcript
 					</a>
 				</div>
 			</section>
-
 		</div>
 	`;
 };
@@ -62,7 +75,7 @@ export const AudioPlayer = () => {
 		miniPlayer: boolean("Mini Player", false),
 		track: {
 			audioFile: "https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3",
-			coverImage: {},
+			coverImage: greekHall1x1,
 			subtitle: text("Subtitle","Praise Songs about Javascript"),
 			title: text("Title", "Track 1. Title"),
 			transcript: text("Transcript", "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpaqui officia deserunt mollit anim id est laborum."),
