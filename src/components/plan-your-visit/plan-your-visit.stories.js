206c908f9b1036cf8d9be3e5bc2cf977a048b678
Joseph Curley <hey.josephcurley@gmail.com>
2021-06-25 16:26:18 -0400

    First pass of CSS for plan card

--- src/components/plan-your-visit/plan-your-visit.stories.js
+++ src/components/plan-your-visit/plan-your-visit.stories.js
@@ -18,8 +18,10 @@ const data = {
 
 const planCardMarkUp = (model) => {
 	return html`<section class="plan-card">
+
 			<div class="plan-card__image">
 			</div>
+
 			<div class="plan-card__info">
 				<h2 class="plan-card__header">${model.header}</h2>
 				<div class="plan-card__details">
@@ -30,14 +32,15 @@ const planCardMarkUp = (model) => {
 							<div class="plan-card__row-header">Dates</div>
 							<div class="plan-card__row-text">${model.dates}</div>
 						</div>
+					</div>
 
-						<div class="plan-card__row">
-							<div class="plan-card__row-icon"></div>
-							<div class="plan-card__row-info">
-								<div class="plan-card__row-header">${model.location}</div>
-								<div class="plan-card__row-text">${model.gallery}</div>
-							</div>
+					<div class="plan-card__row">
+						<div class="plan-card__row-icon"></div>
+						<div class="plan-card__row-info">
+							<div class="plan-card__row-header">${model.location}</div>
+							<div class="plan-card__row-text">${model.gallery}</div>
 						</div>
+					</div>
 
 						<div class="plan-card__row">
 							<div class="plan-card__row-icon"></div>
@@ -48,8 +51,13 @@ const planCardMarkUp = (model) => {
 						</div>
 
 						<div class="plan-card__cta-wrapper">
-							<a class="button button--primary button--small">Plan your visit</a>
-							<a class="button button--tertiary button--small">View all exhbitions</a>
+							<a href="#"
+								class="button primary-button primary-button--small primary-button--filled">
+								Plan your visit
+							</a>
+							<a class="button tertiary-button">
+								View all exhbitions
+							</a>
 						</div>
 					</div>
 				</div>
