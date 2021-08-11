d7bce4758d2283cf0e51ad2358ac82e42b190676
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2021-08-11 13:49:56 -0400

    refactor global lazyload to be idempotent

--- src/global/lazyload/lazyload.js
+++ src/global/lazyload/lazyload.js
@@ -4,32 +4,31 @@ import LazyLoad from "vanilla-lazyload";
 const imageLoadedEvent = new CustomEvent("image-loaded", { bubbles: true });
 const imageErrorEvent = new CustomEvent("image-errored", { bubbles: true });
 
+const callLoadAll = () => {
+	window.marbleLazyLoadObj.loadAll();
+	window.removeEventListener("scroll", callLoadAll, true);
+};
+
 const lazyload = () => {
-	const lazyLoadObj = new LazyLoad({
-		elements_selector: ".lazy",
-		callback_loaded: (el) => {
-			el.dispatchEvent(imageLoadedEvent);
-		},
-		callback_error: (el) => {
-			el.dispatchEvent(imageErrorEvent);
-		},
-	});
+	if (window.marbleLazyLoadObj) {
+		window.marbleLazyLoadObj.update();
+	} else {
+		window.marbleLazyLoadObj =  new LazyLoad({
+			elements_selector: ".lazy",
+			callback_loaded: (el) => {
+				el.dispatchEvent(imageLoadedEvent);
+			},
+			callback_error: (el) => {
+				el.dispatchEvent(imageErrorEvent);
+			},
+		});
 
-	function loadImages() {
-		lazyLoadObj.loadAll();
-		// After they load unbind the scroll listener.
-		window.removeEventListener("scroll", loadImages, true);
+		window.addEventListener("scroll", callLoadAll, {
+			once: true,
+			passive: true,
+			capture: true,
+		});
 	}
-
-	//on scroll load all below the fold Images
-	window.addEventListener("scroll", loadImages, {
-		once: true,
-		passive: true,
-		capture: true,
-	});
-
-	// make this object available globally for future calls
-	window.marbleLazyLoadObj = lazyLoadObj;
 };
 
 export default lazyload;
