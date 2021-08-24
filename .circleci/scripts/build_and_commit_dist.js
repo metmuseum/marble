614d75695e616760d01bdc430577a6caa334ba0c
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2021-08-23 22:51:02 -0400

    refactor buggy git/bash steps to a node script

--- .circleci/scripts/build_and_commit_dist.js
+++ .circleci/scripts/build_and_commit_dist.js
@@ -1,5 +1,31 @@
 const simpleGit = require('simple-git');
 const git = simpleGit();
-git.status((err, status) => { 
-  console.log(status);
-})
+
+const safetyCheck = (err, data) => {
+  // 🚧 safety first
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(data);
+  return data;
+}
+
+const main = async () => {
+  const status = await git.status(safetyCheck);
+  
+  if (status.files.length > 0) {
+    console.log("changes!");
+    await git.add("dist/.", safetyCheck);
+    const headSha = await git.revparse("HEAD", safetyCheck);
+    const branchResult = await git.branch(safetyCheck);
+    const branchName = branchResult.current
+    console.log(branchName);
+    await git.commit(`[skip ci] auto build dist for ${headSha}`, safetyCheck)
+    await git.push("origin", branchName, safetyCheck)
+  } else {
+    console.log("build produced no changes");
+  }
+}
+
+main();
