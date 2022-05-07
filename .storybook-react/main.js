3754b509c157cc4724eb0e9832e82a7155aed889
Stephen Marsh <stephenhmarsh@users.noreply.github.com>
2022-05-06 21:08:41 -0400

    proof of concept of storybook react in the repo

---
+++ .storybook-react/main.js
@@ -0,0 +1,38 @@
+const path = require("path");
+
+module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    config.resolve ||= {}
+    config.resolve.alias ||= {}
+    config.resolve.alias[".storybook"] = path.resolve('.storybook')
+
+    // Return the altered config
+    return config;
+  },
+  "stories": [
+    "../src/**/*.stories.react.mdx",
+    "../src/**/*.stories.react.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    // "@storybook/addon-links",
+    // "@storybook/addon-essentials",
+    // "@storybook/addon-interactions",
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
---
+++ src/index.stories.react.jsx
@@ -0,0 +1,4 @@
+/* eslint storybook/default-exports: "off" */
+import ".storybook/assets/font-declarations.scss";
+import "./marble.scss"; // IMPORTANT: THIS IS THE MAIN ENTRYPOINT OF MARBLE INTO _STORYBOOK_
+// (We need to do it this way so that we get as close to /dist as possible on every story.)
