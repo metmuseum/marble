9f10f56e9a6d2da752825392e14e4d5dd479162d
Joseph Curley <hey.josephcurley@gmail.com>
2022-06-08 13:37:46 -0400

    MVP level

--- src/components/image-viewer/image-viewer.js
+++ src/components/image-viewer/image-viewer.js
@@ -2,6 +2,8 @@ import SETTINGS from "../../global/settings";
 
 function imageViewer(propsObj) {
 
+	const caretIcon = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d='M192 384c-8.188 0-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L192 306.8l137.4-137.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-160 160C208.4 380.9 200.2 384 192 384z'/></svg>";
+	const xIcon = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512'><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d='M376.6 427.5c11.31 13.58 9.484 33.75-4.094 45.06c-5.984 4.984-13.25 7.422-20.47 7.422c-9.172 0-18.27-3.922-24.59-11.52L192 305.1l-135.4 162.5c-6.328 7.594-15.42 11.52-24.59 11.52c-7.219 0-14.48-2.438-20.47-7.422c-13.58-11.31-15.41-31.48-4.094-45.06l142.9-171.5L7.422 84.5C-3.891 70.92-2.063 50.75 11.52 39.44c13.56-11.34 33.73-9.516 45.06 4.094L192 206l135.4-162.5c11.3-13.58 31.48-15.42 45.06-4.094c13.58 11.31 15.41 31.48 4.094 45.06l-142.9 171.5L376.6 427.5z'/></svg>";
 	const imageViewerDefaults= {
 		elSelector: ".js-iv",
 		triggerSelector: ".js-iv__trigger",
@@ -18,11 +20,19 @@ function imageViewer(propsObj) {
 	let modalEl;
 	let fsContentEl;
 	let fsCloseButton;
+	let fsBackButton;
+	let fsForwardButton;
 	let fsContentImageEl;
 	let fsContentTextEl;
 
-	const createModal = () => {
+	let listOfIterableElements;
+	let currentIndex;
 
+	const disableFullScreenView = () => {
+		modalEl.classList.remove("is-fullscreen");
+	};
+
+	const createModal = () => {
 		//Modal Wrapper
 		modalEl = document.createElement("div");
 		modalEl.classList.add("js-fs-container");
@@ -30,12 +40,13 @@ function imageViewer(propsObj) {
 		//Close Button
 		fsCloseButton = document.createElement("button");
 		fsCloseButton.classList.add("js-fs-close");
-		fsCloseButton.innerText = "X";
+		fsCloseButton.innerHTML = xIcon;
 		fsCloseButton.addEventListener("click", (e) => {
 			e.preventDefault();
-			modalEl.classList.remove("is-fullscreen");
+			disableFullScreenView();
 		});
 		modalEl.append(fsCloseButton);
+
 
 		//Content Container
 		fsContentEl = document.createElement("div");
@@ -50,10 +61,30 @@ function imageViewer(propsObj) {
 		fsContentTextEl.classList.add("js-fs-text");
 		fsContentEl.append(fsContentTextEl);
 
+		//Back Button
+		fsBackButton = document.createElement("button");
+		fsBackButton.classList.add("js-fs-back");
+		fsBackButton.innerHTML = caretIcon;
+		fsBackButton.addEventListener("click", (e) => {
+			e.preventDefault();
+			iterate(false);
+		});
+		modalEl.append(fsBackButton);
+
+		//Forward Button
+		fsForwardButton = document.createElement("button");
+		fsForwardButton.classList.add("js-fs-forward");
+		fsForwardButton.innerHTML = caretIcon;
+		fsForwardButton.addEventListener("click", (e) => {
+			e.preventDefault();
+			iterate(true);
+		});
+
+
+		modalEl.append(fsForwardButton);
+
 		document.body.append(modalEl);
 	};
-
-	createModal();
 
 	const populateModal = (el) => {
 		const img = el.querySelector(props.imageSelector).cloneNode(true);
@@ -68,19 +99,48 @@ function imageViewer(propsObj) {
 		fsContentImageEl.append(img);
 	};
 
-	const enableFullScreenView = (el) => {
-		console.log(el);
+	const iterate = (forward) => {
+		if (forward) {
+			if (currentIndex < listOfIterableElements.length - 1) {
+				currentIndex++;
+				populateModal(listOfIterableElements[currentIndex]);
+			}
+		} else if (currentIndex > 0) {
+			currentIndex--;
+			populateModal(listOfIterableElements[currentIndex]);
+		}
+	};
+
+	const bindArrowKeys = () => {
+		document.addEventListener("keydown", (e) => {
+			if (e.keyCode == "37") {
+				if (currentIndex > 0) {
+					iterate(false);
+				}
+			} else if (e.keyCode == "39") {
+				if (currentIndex < listOfIterableElements.length - 1) {
+					iterate(true);
+				}
+			}
+		});
+	};
+
+	const enableFullScreenView = (el, index, listOfElements) => {
+		listOfIterableElements = listOfElements;
+		currentIndex= index;
 		populateModal(el);
+		bindArrowKeys();
 		modalEl.classList.add("is-fullscreen");
 	};
 
 	const initializeIVels = (ivContainer) => {
-		ivContainer.querySelectorAll(props.elSelector)
-			.forEach((ivEl) => {
+		const listOfElements = ivContainer.querySelectorAll(props.elSelector);
+		listOfElements
+			.forEach((ivEl, index) => {
 				ivEl.querySelector(props.triggerSelector)
 					.addEventListener("click", (e) => {
 						e.preventDefault();
-						enableFullScreenView(ivEl);
+						enableFullScreenView(ivEl, index, listOfElements);
 					});
 			});
 	};
@@ -90,6 +150,8 @@ function imageViewer(propsObj) {
 			ivContainer.classList.add(SETTINGS.initializedClassName);
 			initializeIVels(ivContainer);
 		});
+
+	createModal();
 }
 
 export default imageViewer;
--- src/components/image-viewer/image-viewer.stories.js
+++ src/components/image-viewer/image-viewer.stories.js
@@ -33,7 +33,13 @@ const imageViewerMarkUp = (imageTitle) => {
 				/>
 				<div class="js-iv__caption">
 					<h1>${imageTitle}</h1>
-					<p>Lorem Ipsum Dolar Sit Amet</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse aliquam justo ut nunc fringilla ornare a eget diam. Vestibulum ornare turpis vel porta ultrices. In egestas ipsum est. Sed turpis nunc, pretium eleifend mattis eget, maximus non nulla. Vestibulum sagittis velit felis, at porttitor dui vehicula a. Phasellus ut tempor sapien. Cras a tempus lorem. Duis metus nulla, consectetur efficitur est nec, mollis lobortis ligula.
+
+Curabitur auctor scelerisque metus in commodo. Nunc interdum eros felis, ac interdum mauris euismod eu. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec a justo ligula. Phasellus id nibh et urna ultricies pharetra et nec metus. Ut malesuada gravida metus, vitae tincidunt augue venenatis nec. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vitae volutpat mi. Duis elementum elit sed felis euismod, eget facilisis leo malesuada. Donec vestibulum dictum neque, ornare eleifend lacus commodo sit amet. Suspendisse potenti. Donec semper euismod velit et ornare. Curabitur aliquam a quam eu tincidunt. In porttitor et nulla molestie bibendum. Nulla sed purus dapibus, facilisis lorem sit amet, iaculis felis.
+
+Nunc consectetur eget justo sed efficitur. Suspendisse sit amet augue vel lacus cursus feugiat vitae sed tortor. Nulla sed vestibulum lectus. Sed euismod justo vel lectus rutrum, a laoreet magna auctor. Pellentesque et tellus nulla. Aenean mi nisl, varius ut suscipit vitae, dapibus eget quam. Curabitur accumsan fermentum elit, in sollicitudin orci varius non. Nunc pharetra ante sit amet metus hendrerit, eu consequat neque consequat. Quisque id sapien elementum, rutrum purus vel, gravida lacus. Pellentesque et justo congue, venenatis augue eget, cursus orci.
+
+Vivamus elementum congue cursus. Aenean nunc nisl, consequat non tellus vitae, fermentum vehicula ligula. Sed dapibus dolor nec libero ultrices, nec auctor lorem consectetur. Morbi libero enim, accumsan luctus elit quis, pulvinar hendrerit nulla. Nullam id enim vel libero accumsan consectetur et vel nisi. Nunc pharetra risus vel libero sodales, vitae vehicula dolor venenatis. Nam ac tincidunt turpis. Pellentesque eleifend massa vitae vulputate egestas.</p>
 				</div>
 			</div>
 		</div>
