1b30d26ee830ab5b29b786155c0d1f507aa08b47
David Seguin <david.seguin@gmail.com>
2022-10-19 13:54:39 -0400

    DIGCOLL-5610 cards and layout

--- src/components/browse-by/browse-by.stories.js
+++ src/components/browse-by/browse-by.stories.js
@@ -1,107 +1,181 @@
 import { html } from ".storybook/helpers";
 import { useEffect } from "@storybook/client-api";
 import browseByjs from "./browse-by.js";
-import greekHallImages from ".storybook/assets/images/greek-hall";
 
 export default { title: "Components/BrowseBy" };
 
-const titles = [
-	"American Art",
-	"Ancient American Art",
-	"Ancient Near Eastern Art",
-	"Ancient Near Eastern Art",
-	"Arms and Armor",
-	"Asian Art",
-	"The Costume Institute",
-	"Drawings and Prints",
-	"Egyptian Art",
-	"European Paintings",
-	"European Sculpture and Decorative Arts",
-	"Greek and Roman Art",
-	"Islamic Art",
-	"The Robert Lehman Collection",
-	"Modern and Contemporary Art"
-];
-
-const colors = [
-	"#abdbe3",
-	"#eeeee4",
-	"#f8e3c9"
-];
-
-const images = [greekHallImages.image1x1, greekHallImages.image4x3, greekHallImages.image16x9];
-
-// these items can literally be any card
-const cardTemplate = (args) => {
-	return html`
-		<div class="marble-card" style="background:${args.color}">
-			<a href="link here" class="marble-card__image-link" tabindex="-1">
-				<div class="marble-card__image-wrapper">
-					<img class="marble-card__image" src="${args.imageURL}" alt="an example image alt for accessibility" />
-				</div>
-			</a>
-
-			<div class="marble-card__subject">
-				<h4 class="marble-card__header">
-					<a href="a link">
-						${args.title}
-					</a>
-				</h4>
-				<div class="marble-card__meta">
-					<div class="marble-card__meta-body">
-						<div class="marble-card__meta-description">
-							A description
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>`;
-};
-
 export const BrowseBy = () => {
-	const screenreaderLegendText = "Use your arrow keys to navigate the topics in the tabs below, and your tab key to choose an item";
-	const tabNames = ["topic1", "topic2", "topic3"];
-	const inputGroupName = "browseby-tabset";
+
 	useEffect(browseByjs);
 	return html`
 	<section class="browseby js-browseby">
-		<fieldset class="tabs-control-container js-tabs-control-container">
-		<legend class="screen-reader-only">${screenreaderLegendText}</legend>
-		${tabNames.map((tabName, index) => {
-		let niceTabName = tabName.replace(/.{1}$/," $&"); //just make it a bit more readable for storybook
-		let valueForChecked ="";
-		if (index === 0) {
-			valueForChecked = "checked";
-		}
-		return html`
-			<div class="tab-controls">
-				<input
-					id="${tabName}-tab"
-					type="radio"
-					name=${inputGroupName}
-					class="tab-controls__input js-browseby-tab"
-					value="${tabName}"
-					${valueForChecked}
-				/>
-				<label for="${tabName}-tab" class="tab-controls__label">
-					<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> ${niceTabName}</h3>
-				</label>
-			</div>
-		`;}).join("")}
-		</fieldset>
-
-		${tabNames.map((tabName, index) => {
-		let randomImageSrc = Math.floor(Math.random() * 3) + 1; //just randomize this a bit for storybook
-		let randomColorSrc = Math.floor(Math.random() * 3) + 1; //just randomize this a bit for storybook
-		let niceTabName = tabName.replace(/.{1}$/," $&"); //just make it a bit more readable for storybook
-		let valueForHidden = index === 0 ? "" : "hidden";
-		return html`
-			<section id="${tabName}" class="browseby-tabpanel js-browseby-tabpanel" ${valueForHidden}>
-			<h3 class="screen-reader-only">${niceTabName}</h3>
-				<div class="browseby-tabpanel-body">
-					${titles.map((title, index) => cardTemplate({ title: title, color: colors[index % randomColorSrc], imageURL: images[index % randomImageSrc].srcSet.fallback })).join("")}
+		<div class="browseby-header">
+			<span class="browseby-header__title">Browse by: </span>
+			<fieldset class="tabs-control-container js-tabs-control-container">
+				<legend class="screen-reader-only">Use your arrow keys to navigate the topics in the tabs below, and your tab key to choose an item</legend>
+				<div class="tab-controls">
+					<input id="topic1-tab" type="radio" name="browseby-tabset" class="tab-controls__input js-browseby-tab" value="topic1" checked>
+					<label for="topic1-tab" class="tab-controls__label">
+						<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> Topic One</h3>
+					</label>
 				</div>
-			</section>
-		`;}).join("")}
+				<div class="tab-controls">
+					<input id="topic2-tab" type="radio" name="browseby-tabset" class="tab-controls__input js-browseby-tab" value="topic2">
+					<label for="topic2-tab" class="tab-controls__label">
+						<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> Topic Two</h3>
+					</label>
+				</div>
+				<div class="tab-controls">
+					<input id="topic3-tab" type="radio" name="browseby-tabset" class="tab-controls__input js-browseby-tab" value="topic3">
+					<label for="topic3-tab" class="tab-controls__label">
+						<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> Topic Three</h3>
+					</label>
+				</div>
+			</fieldset>
+		</div>
+
+		<section id="topic1" class="browseby-tabpanel js-browseby-tabpanel">
+			<h3 class="screen-reader-only">Topic One</h3>
+			<div class="browseby-tabpanel-body">
+				<div class="article-card">
+					<div class="article-card__image-wrapper">
+						<a href="link here" class="article-card__image-link" tabindex="-1">
+							<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/1x1/1240.jpg" alt="an example image alt for accessibility">
+							</a>
+					</div>
+					<div class="article-card__subject">
+						<h4 class="article-card__header-heading">
+							<a href="http://www.metmuseum.org">
+								American Art
+							</a>
+						</h4>
+					</div>
+				</div>
+				<div class="article-card">
+					<a href="link here" class="article-card__image-link" tabindex="-1">
+						<div class="article-card__image-wrapper">
+							<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/4x3/1240.jpg" alt="an example image alt for accessibility">
+						</div>
+					</a>
+					<div class="article-card__subject">
+						<h4 class="article-card__header-heading">
+							<a href="http://www.metmuseum.org">
+								Ancient American Art
+							</a>
+						</h4>
+					</div>
+				</div>
+				<div class="article-card">
+					<a href="link here" class="article-card__image-link" tabindex="-1">
+						<div class="article-card__image-wrapper">
+							<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/1x1/1240.jpg" alt="an example image alt for accessibility">
+						</div>
+					</a>
+					<div class="article-card__subject">
+						<h4 class="article-card__header-heading">
+							<a href="http://www.metmuseum.org">
+								Ancient Near Eastern Art
+							</a>
+						</h4>
+					</div>
+				</div>
+			</div>
+		</section>
+	
+	<section id="topic2" class="browseby-tabpanel js-browseby-tabpanel" hidden="">
+		<h3 class="screen-reader-only">Topic Two</h3>
+		<div class="browseby-tabpanel-body">
+			<div class="article-card">
+				<a href="link here" class="article-card__image-link" tabindex="-1">
+					<div class="article-card__image-wrapper">
+						<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/4x3/1240.jpg" alt="an example image alt for accessibility">
+					</div>
+				</a>
+				<div class="article-card__subject">
+					<h4 class="article-card__header-heading">
+						<a href="http://www.metmuseum.org">
+							Greek and Roman Art
+						</a>
+					</h4>
+				</div>
+			</div>
+			<div class="article-card">
+				<a href="link here" class="article-card__image-link" tabindex="-1">
+					<div class="article-card__image-wrapper">
+						<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/1x1/1240.jpg" alt="an example image alt for accessibility">
+					</div>
+				</a>
+				<div class="article-card__subject">
+					<h4 class="article-card__header-heading">
+						<a href="http://www.metmuseum.org">
+							Islamic Art
+						</a>
+					</h4>
+				</div>
+			</div>
+			<div class="article-card">
+				<a href="link here" class="article-card__image-link" tabindex="-1">
+					<div class="article-card__image-wrapper">
+						<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/4x3/1240.jpg" alt="an example image alt for accessibility">
+					</div>
+				</a>
+				<div class="article-card__subject">
+					<h4 class="article-card__header-heading">
+						<a href="http://www.metmuseum.org">
+							The Robert Lehman Collection
+						</a>
+					</h4>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section id="topic3" class="browseby-tabpanel js-browseby-tabpanel" hidden="">
+		<h3 class="screen-reader-only">Topic Three</h3>
+		<div class="browseby-tabpanel-body">
+			<div class="article-card">
+				<a href="link here" class="article-card__image-link" tabindex="-1">
+					<div class="article-card__image-wrapper">
+						<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/4x3/1240.jpg" alt="an example image alt for accessibility">
+					</div>
+				</a>
+				<div class="article-card__subject">
+					<h4 class="article-card__header-heading">
+						<a href="http://www.metmuseum.org">
+							Asian Art
+						</a>
+					</h4>
+				</div>
+			</div>
+			<div class="article-card">
+			<a href="link here" class="article-card__image-link" tabindex="-1">
+				<div class="article-card__image-wrapper">
+					<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/4x3/1240.jpg" alt="an example image alt for accessibility">
+				</div>
+			</a>
+			<div class="article-card__subject">
+				<h4 class="article-card__header-heading">
+					<a href="http://www.metmuseum.org">
+						Drawings and Prints
+					</a>
+				</h4>
+			</div>
+		</div>
+			<div class="article-card">
+				<a href="link here" class="article-card__image-link" tabindex="-1">
+					<div class="article-card__image-wrapper">
+						<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/1x1/1240.jpg" alt="an example image alt for accessibility">
+					</div>
+				</a>
+				<div class="article-card__subject">
+					<h4 class="article-card__header-heading">
+						<a href="http://www.metmuseum.org">
+							The Costume Institute
+						</a>
+					</h4>
+				</div>
+			</div>
+		</div>
+	</section>
 	</section>`;
 };
