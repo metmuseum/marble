08ce6e92f3141068f9f81ce9dc0dbd03e08d334c
David Seguin <david.seguin@gmail.com>
2022-10-20 17:26:47 -0400

    DIGCOLL-5610 dropdown

--- src/components/browse-by/browse-by.stories.js
+++ src/components/browse-by/browse-by.stories.js
@@ -13,19 +13,19 @@ export const BrowseBy = () => {
 			<span class="browseby-header__title">Browse by: </span>
 			<fieldset class="browseby-tabs-control-container tabs-control-container js-tabs-control-container">
 				<legend class="screen-reader-only">Use your arrow keys to navigate the topics in the tabs below, and your tab key to choose an item</legend>
-				<div class="tab-controls">
+				<div class="tab-controls browseby-tab-controls js-tab-controls highlighted-tab">
 					<input id="topic1-tab" type="radio" name="browseby-tabset" class="tab-controls__input js-browseby-tab" value="topic1" checked>
 					<label for="topic1-tab" class="tab-controls__label">
 						<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> Topic One</h3>
 					</label>
 				</div>
-				<div class="tab-controls">
+				<div class="tab-controls browseby-tab-controls js-tab-controls">
 					<input id="topic2-tab" type="radio" name="browseby-tabset" class="tab-controls__input js-browseby-tab" value="topic2">
 					<label for="topic2-tab" class="tab-controls__label">
 						<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> Topic Two</h3>
 					</label>
 				</div>
-				<div class="tab-controls">
+				<div class="tab-controls browseby-tab-controls js-tab-controls">
 					<input id="topic3-tab" type="radio" name="browseby-tabset" class="tab-controls__input js-browseby-tab" value="topic3">
 					<label for="topic3-tab" class="tab-controls__label">
 						<h3 class="tab-controls__heading"><span class="screen-reader-only">Show panel </span> Topic Three</h3>
@@ -106,7 +106,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-				
 				<div class="article-card">
 					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
 						<a href="link here" class="article-card__image-link" tabindex="-1">
@@ -121,7 +120,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-
 				<div class="article-card">
 					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
 						<a href="link here" class="article-card__image-link" tabindex="-1">
@@ -150,7 +148,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-
 				<div class="article-card">
 					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
 						<a href="link here" class="article-card__image-link" tabindex="-1">
@@ -165,7 +162,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-				
 				<div class="article-card">
 					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
 						<a href="link here" class="article-card__image-link" tabindex="-1">
@@ -180,7 +176,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-				
 			</div>
 		</section>
 	
@@ -201,7 +196,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-
 				<div class="article-card">
 					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
 						<a href="link here" class="article-card__image-link" tabindex="-1">
@@ -216,7 +210,6 @@ export const BrowseBy = () => {
 						</h4>
 					</div>
 				</div>
-
 				<div class="article-card">
 					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
 						<a href="link here" class="article-card__image-link" tabindex="-1">
@@ -227,6 +220,20 @@ export const BrowseBy = () => {
 						<h4 class="article-card__header-heading">
 							<a href="http://www.metmuseum.org">
 								The Robert Lehman Collection
+							</a>
+						</h4>
+					</div>
+				</div>
+				<div class="article-card">
+					<div class=" article-card__image-wrapper article-card__image-wrapper--4by3">
+						<a href="link here" class="article-card__image-link" tabindex="-1">
+							<img class="article-card__image" src="static/media/.storybook/assets/images/greek-hall/1x1/1240.jpg" alt="an example image alt for accessibility">
+						</a>
+					</div>
+					<div class="article-card__subject">
+						<h4 class="article-card__header-heading">
+							<a href="http://www.metmuseum.org">
+								American Art
 							</a>
 						</h4>
 					</div>
