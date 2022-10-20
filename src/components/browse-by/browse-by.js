e3002931670acd1dfcfd0269c37662bcb611f344
David Seguin <david.seguin@gmail.com>
2022-10-20 17:26:47 -0400

    DIGCOLL-5610 dropdown

--- src/components/browse-by/browse-by.js
+++ src/components/browse-by/browse-by.js
@@ -5,6 +5,8 @@ function browseBy() {
 		browseBys.forEach((browseBy)=> {
 			let tabsContainer = browseBy.querySelector(".js-tabs-control-container");
 			let tabs = tabsContainer.querySelectorAll(".js-browseby-tab");
+			let allTabPanels = browseBy.querySelectorAll(".js-browseby-tabpanel");
+			let tabControls = tabsContainer.querySelectorAll(".js-tab-controls");
 	
 			tabs.forEach((tab) => {
 				tab.addEventListener("click", function () {
@@ -22,25 +24,39 @@ function browseBy() {
 			};
 
 			const closeAllTabPanels = () => {
-				let allTabPanels = browseBy.querySelectorAll(".js-browseby-tabpanel");
 				allTabPanels.forEach((tabPanel) => {
 					tabPanel.setAttribute("hidden", true);
 				});
 			};
 
 			const handleDropDownModeClick = (tabClicked) => {
-				if (tabsContainer.classList.contains("tab-controls--open")) {
+				if (tabsContainer.classList.contains("browseby-tab-controls--open")) {
 					openTabPanel(tabClicked);
 				} else {
-					tabsContainer.classList.add("tab-controls--open");
+					tabsContainer.classList.add("browseby-tab-controls--open");
 				}
+			};
+
+			const reorderDropDown = (tabClicked) => {
+				if (tabsContainer.classList.contains("browseby-tab-controls--open")) {
+					tabsContainer.classList.remove("browseby-tab-controls--open");
+					deHighlightAllTabContainers();
+					tabClicked.parentNode.classList.add("highlighted-tab");
+				}
+			};
+			
+			const deHighlightAllTabContainers = () => {
+				tabControls.forEach((tabControl) => {
+					tabControl.classList.remove("highlighted-tab");
+				});
 			};
 
 			const openTabPanel = (tabClicked) => {
 				let clickedID = tabClicked.id;
 				let IDofTabPanelToOpen = clickedID.substring(0, clickedID.length - 4);
 				let tabPanelToOpen = document.getElementById(IDofTabPanelToOpen);
-				closeAllTabPanels();
+				reorderDropDown(tabClicked);
+				closeAllTabPanels(tabClicked);
 				tabPanelToOpen.removeAttribute("hidden");
 			};
 		});
