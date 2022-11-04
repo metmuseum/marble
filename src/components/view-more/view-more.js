c1ab6c24c00b7ea3659d303b41338d1776294cd2
David Seguin <david.seguin@gmail.com>
2022-11-04 12:58:14 -0400

    Stephen feedback contd

--- src/components/view-more/view-more.js
+++ src/components/view-more/view-more.js
@@ -6,62 +6,59 @@ const viewMore = () => {
 	}
 };
 
+const checkTabpanelHeight = (tabpanelToCheck) => {
+	let tabpanelToCheckInner = tabpanelToCheck.querySelector(".js-view-more-panel-body");
+	if (tabpanelToCheckInner.clientHeight < tabpanelToCheckInner.scrollHeight) {
+		tabpanelToCheck.classList.add("view-more-panel--cropped");
+	} else {
+		tabpanelToCheck.classList.remove("view-more-panel--cropped");
+	}
+};
+
+const expandTabpanelText = (tabToExpand, buttonClicked) => {
+	buttonClicked.querySelector(".js-view-more-panel-cta-text").innerHTML = "Less";
+	tabToExpand.classList.remove("view-more-panel--cropped");
+	tabToExpand.classList.add("view-more-panel--expanded");
+};
+
+const contractTabpanelText  = (viewMore, tabToContract, buttonClicked) => {
+	window.scrollTo({
+		behavior: "smooth",
+		top: viewMore.getBoundingClientRect().top - document.body.getBoundingClientRect().top - 80,
+	});
+	tabToContract.addEventListener("transitionend", function () {
+		buttonClicked.querySelector(".js-view-more-panel-cta-text").innerHTML = "View more";
+	}, {
+		once: true
+	});
+	tabToContract.classList.add("view-more-panel--cropped");
+	tabToContract.classList.remove("view-more-panel--expanded");
+};
+
+const expandOrContractTabpanel = (viewMore, buttonClicked) => {
+	let thePanel = buttonClicked.closest(".js-view-more-panel");
+	if (thePanel.classList.contains("view-more-panel--cropped")) {
+		expandTabpanelText(thePanel, buttonClicked);
+	} else {
+		contractTabpanelText(viewMore, thePanel, buttonClicked);
+	}
+};
+
 const initialize = () => {
 	const viewMores = document.querySelectorAll(".js-view-more-panel");
 	const mobileBreakpoint = SETTINGS.mobileBreakpoint;
 
 	viewMores.forEach((viewMore)=> {
-		let panelButton = viewMore.querySelectorAll(".js-view-more-panel-cta");
-		let isMobileOnly = viewMore.classList.contains("js-view-more-panel--mobile-only") ? true : false;
-		let windowWidth = window.innerWidth;
+		let panelButtons = viewMore.querySelectorAll(".js-view-more-panel-cta");
+		let isMobileOnly = viewMore.classList.contains("js-view-more-panel--mobile-only");
 
-		const checkTabpanelHeight = (tabpanelToCheck) => {
-			let tabpanelToCheckInner = tabpanelToCheck.querySelector(".js-view-more-panel-body");
-			if (tabpanelToCheckInner.clientHeight < tabpanelToCheckInner.scrollHeight) {
-				tabpanelToCheck.classList.add("view-more-panel--cropped");
-			} else {
-				tabpanelToCheck.classList.remove("view-more-panel--cropped");
-			}
-		};
-		if (!isMobileOnly || (windowWidth <= mobileBreakpoint)) {
+		if (!isMobileOnly || (window.innerWidth <= mobileBreakpoint)) {
 			checkTabpanelHeight(viewMore);
 		}
-		
-		const expandTabpanelText = (tabToExpand, buttonClicked) => {
-			buttonClicked.querySelector(".js-view-more-panel-cta-text").innerHTML = "Less";
-			tabToExpand.classList.remove("view-more-panel--cropped");
-			tabToExpand.classList.add("view-more-panel--expanded");
-		};
 
-		const contractTabpanelText  = (tabToContract, buttonClicked) => {
-			window.scrollTo({
-				behavior: "smooth",
-				top: viewMore.getBoundingClientRect().top - document.body.getBoundingClientRect().top - 80,
-			});
-			tabToContract.addEventListener("transitionend", function () {
-				buttonClicked.querySelector(".js-view-more-panel-cta-text").innerHTML = "View more";
-			}, {
-				once: true
-			});
-			tabToContract.classList.add("view-more-panel--cropped");
-			tabToContract.classList.remove("view-more-panel--expanded");
-		};
-
-		const handleCTAclick = (buttonClicked) => {
-			let theTab = buttonClicked.closest(".js-view-more-panel");
-			if (theTab.classList.contains("view-more-panel--cropped")) {
-				expandTabpanelText(theTab, buttonClicked);
-			} else {
-				contractTabpanelText(theTab, buttonClicked);
-			}
-		};
-
-		panelButton.forEach((showMoreCTA) => {
-			showMoreCTA.addEventListener("click", function (e) {
-				e.preventDefault();
-				handleCTAclick(showMoreCTA);
-			});
-		});
+		panelButtons.forEach((panelButton) => panelButton.addEventListener("click", function(){
+			expandOrContractTabpanel(viewMore, panelButton);
+		}));
 	});
 };
 
