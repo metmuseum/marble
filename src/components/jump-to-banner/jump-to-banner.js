e42c29f3d7d4028e366de81f0d1f77e116083454
David Seguin <david.seguin@gmail.com>
2022-12-09 16:18:56 -0500

    DIGCOLL-5750 Marsh version of this js

--- src/components/jump-to-banner/jump-to-banner.js
+++ src/components/jump-to-banner/jump-to-banner.js
@@ -1,29 +1,13 @@
+const handleLinkClick = (e) => {
+	e.preventDefault();
+	const idFromURL = new URL(e.target.href).hash.substring(1);
+	document.getElementById(idFromURL)?.scrollIntoView({ behavior: "smooth" });
+};
+
 const jumpToBanner = () => {
-	if (document.querySelector(".js-jump-to")) {
-		const jumpToBanners = document.querySelectorAll(".js-jump-to");
-
-		jumpToBanners.forEach((jumpToBanner)=> {
-			let jumpToLinks = jumpToBanner.querySelectorAll(".js-jump-to__link");
-	
-			jumpToLinks.forEach((jumpToLink) => {
-				jumpToLink.addEventListener("click", function (e) {
-					e.preventDefault();
-					handleLinkClick(jumpToLink);
-				});
-			});
-
-			const handleLinkClick = (jumpToLink) => {
-				console.log("link clicked", jumpToLink);
-				let urlFromLink = new URL(jumpToLink.href);
-				let idFromURL = urlFromLink.hash.substring(1);
-				let sectionToScrollTo = document.getElementById(idFromURL);
-				window.scrollTo({
-					behavior: "smooth",
-					top: sectionToScrollTo.getBoundingClientRect().top - document.body.getBoundingClientRect().top,
-				});
-			};
-		});
-	}
+	document.querySelectorAll(".js-jump-to__link").forEach((jumpToLink) => {
+		jumpToLink.addEventListener("click", handleLinkClick);
+	});
 };
 
 export default jumpToBanner;
